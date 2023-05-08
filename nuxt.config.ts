a8609c2a408fb84931f814cef18345d7f3a65c75
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2023-05-07 20:37:02 -0400

    Add basic meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+      title: 'Gloria Rodríguez Fernández',
+      meta: [
+        { name: 'description', content: 'Frontend Developer. Never stop learning. Animal lover. Sports' }
+      ],
+    },
+  },
+});
